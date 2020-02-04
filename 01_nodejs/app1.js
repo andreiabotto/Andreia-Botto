b86e3cb8d9aaebed5c8d9e9fcc797feccb11cc1b
Andreia <andreia.botto@hotmail.com>
2020-02-03 21:03:01 -0300

    Aula 01 - Introdução ao node

---
+++ 01_nodejs/app1.js
@@ -0,0 +1,4 @@
+var fn = require('./funcoes');
+
+var soma = fn(2, 7);
+console.log('Soma: ' + soma);
---
+++ 01_nodejs/funcoes.js
@@ -0,0 +1,10 @@
+// modulo unico
+
+function calcular(a,b) {
+    return a + b;
+}
+module.exports = function(x,y){
+    return calcular(x,y);
+}
+
+
